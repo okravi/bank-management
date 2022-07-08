673a1830a7069b6805430cfa4eac89446e7cbfc3
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-07-08 12:48:42 +0300

    progress update

---
+++ src/main/model/account/Account.java
@@ -0,0 +1,59 @@
+package src.main.model.account;
+
+abstract class Account {
+    String id;
+    String name;
+    double balance;
+
+    public Account(String id, String name, double balance) throws IllegalArgumentException{
+
+        if (id == null || id.isBlank() || name == null || name.isBlank())
+        throw new IllegalArgumentException("Fields can not be null or blank");  
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+    
+    public Account(Account source) {
+        this.id = source.id;
+        this.name = source.name;
+        this.balance = source.balance;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId (String id) throws IllegalArgumentException{
+        if (id == null || id.isBlank())
+        throw new IllegalArgumentException("Fields can not be null or blank");  
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) throws IllegalArgumentException{
+        if (name == null || name.isBlank())
+        throw new IllegalArgumentException("Fields can not be null or blank");  
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return (this.getClass().getSimpleName()) + "    " +
+        "\t" + id + "" +
+        "\t" + name + "" +
+        "\t$" + balance + "";
+    }
+
+}
