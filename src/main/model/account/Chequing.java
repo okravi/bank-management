1d8188641efec881b407a83d122a0032c96bb89a
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-07-08 12:49:13 +0300

    progress update

--- src/main/model/account/Chequing.java
+++ src/main/model/account/Chequing.java
@@ -1,6 +1,14 @@
 package src.main.model.account;
 
-public class Chequing { 
+public class Chequing extends Account{
+
+    public Chequing(String id, String name, double balance) {
+        super(id, name, balance);
+    } 
+
+    public Chequing(Chequing source) {
+        super(source);
+    } 
 
 
 }
--- src/main/model/account/Savings.java
+++ src/main/model/account/Savings.java
@@ -1,7 +1,14 @@
 package src.main.model.account;
 
-public class Savings {
+public class Savings extends Account{
 
+    public Savings(String id, String name, double balance) {
+        super(id, name, balance);
+    } 
+
+    public Savings(Savings source) {
+        super(source);
+    } 
 
 
 }
