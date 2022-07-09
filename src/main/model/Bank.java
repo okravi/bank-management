5e2b57f71ea1d8070c8e05fd8281f44304902b3a
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-07-09 14:10:59 +0300

    progress update

--- src/main/model/Bank.java
+++ src/main/model/Bank.java
@@ -1,7 +1,23 @@
 package src.main.model;
 
+import java.util.ArrayList;
+
+import src.main.model.account.Account;
+
 public class Bank {
+    
+    private ArrayList<Account> accounts;
+    private ArrayList<Transaction> transactions;
 
 
-  
+    public Bank() {
+        this.accounts = new ArrayList<Account>(1);
+        this.transactions = new ArrayList<Transaction>(1);
+    }
+
+    public void addAccount(Account account) {
+        this.accounts.add(account.clone());
+    }
+
+    
 }
